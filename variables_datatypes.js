5f3aef5a704e17c2792ca0776be371855debcf9d
mariamaelmi <151776162+mariamaelmi@users.noreply.github.com>
2024-02-21 17:51:38 +0000

    first commit

---
+++ variables_datatypes.js
@@ -0,0 +1,22 @@
+var firstNameOfTheStudent = "mariama"
+console.log(FirstName)
+ 
+// var lastName ='elmi'
+// console.log(lastName)
+
+// var age = 8
+// console.log(age)
+
+// var isStudent = false
+// console.log(isStudent)
+
+// console.log(typeof(isStudent))
+
+// var name = ["lucy","amy"]
+
+// console.log(name)
+
+// var salary = 300
+// var tax = 30
+// var takeHomeSalary = salary - tax 
+// console.log (takeHomeSalary) 
